adc4f4e6abe44b6b2598b8ad5a3ef218a9b491eb
vuyohlongwane <57765696+vuyohlongwane@users.noreply.github.com>
2020-03-28 19:21:06 +0200

    Made initial file without solver
    Stored data and constraints information.

---
+++ Ntsika project.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 18:41:36 2020
+
+@author: Vuyo-Minenhle.Hlongw
+"""
+
+import numpy as np
+
+Investments={
+        'Ntsika':[256508, 22055, 111905, 2917, 0],
+        'Refiloe':[7200, 21170, 0, 0, 0], 
+        'Thando':[0, 0, 0, 0, 0],
+        'Lerato':[0, 0, 0, 0, 0], 
+        }
