35e539735ddf0a97c04c9e9c49d1149abe8a29c5
Mutahi Wachira <>
2020-03-28 19:36:01 +0200

    Update Ntsika project.py

--- Ntsika project.py
+++ Ntsika project.py
@@ -2,7 +2,6 @@
 """
 Created on Sat Mar 28 18:41:36 2020
 
-@author: Vuyo-Minenhle.Hlongw
 """
 
 import numpy as np
@@ -10,6 +9,6 @@ import numpy as np
 Investments={
         'Ntsika':[256508, 22055, 111905, 2917, 0],
         'Refiloe':[7200, 21170, 0, 0, 0], 
-        'Thando':[0, 0, 0, 0, 0],
+        'Thando':[200, 0, 0, 0, 0],
         'Lerato':[0, 0, 0, 0, 0], 
         }
