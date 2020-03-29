5422b0be5f5da476c2523a35aeca11a8bdc23b17
Mutahi Wachira <>
2020-03-29 22:36:33 +0200

    added RA deduction logic

--- Ntsika project.py
+++ Ntsika project.py
@@ -5,9 +5,11 @@ Created on Sat Mar 28 18:41:36 2020
 @author: Vuyo-Minenhle.Hlongw
 """
 
+# %% Import Statements
 import numpy as np
 from scipy.optimize import minimize
 
+# %% Initial Variables
 Income={
 #        'Heading':['Salary', 'Rental', 'Commission', 'Dividends', 'Interest', 'CapitalGain'],
         'Ntsika':[256508, 22055, 111905, 2917, 0,0],
@@ -24,10 +26,22 @@ Funds={
        'ME':[0, 55000, 0, 480000, 28000],
        }
 
+RA_contributions={
+        'Ntsika': 600*12,
+        'Refiloe': 300*12,
+        'Thando': 300*12,
+        'Lerato': 300*12
+        }
+
+# %% Function Definitions
+
+def deduction_ra(ra_contribution, taxable_income):
+    return min(ra_contribution, 0.275*taxable_income, 350000)
+
 def tax(annual_income):
     '''returns tax rate and contribution 
     for an individual's annual income'''
-    if 4000<annual_income<189880 :
+    if 14220<annual_income<189880 :
         rate=0.18
         cont=(annual_income)*rate+0
     elif 189881<annual_income<296540 :
@@ -57,28 +71,21 @@ def tax(annual_income):
     
     return [rate, cont]  
 
-
 def obj_func(x):
 #   Here is what x stands for:
 #   KC1+ KC2+ KC3+ KC4+ NC1+ NC2+ NC3+ NC4+ ME1+ ME2+ ME3+  ME4
 #   x[0]+x[1]+x[2]+x[3]+x[4]+x[5]+x[6]+x[7]+x[8]+x[9]+x[10]+x[11]
-    Ntsika=tax(sum(Income['Ntsika'])+x[4]+x[8])[1]    
-    Refiloe=tax(sum(Income['Refiloe'])+x[1]+x[5]+x[9])[1]
+    Ntsika=tax(sum(Income['Ntsika'])+x[4]+x[8])[1]
+    Refiloe=tax(sum(Income['Refiloe'])+x[1]+x[5]+x[9])[1]    
     Thando=tax(sum(Income['Thando'])+x[2]+x[10])[1]
     Lerato=tax(sum(Income['Lerato'])+x[11])[1]
     
     total_tax=Ntsika+Refiloe+Thando+Lerato
-    
-    
-    
     return total_tax
-
-
 
 x0=np.array([0, sum(Funds['KC']), 0, 0, 0, sum(Funds['NC']), 0, 0, sum(Funds['ME']), 0, 0, 0])# initial guess
 
 #define constraints
-
 def constraint1(x):
     return x[0]+x[1]+x[2]+x[3]-sum(Funds['KC'])
 
@@ -102,14 +109,12 @@ con3={'type':'eq','fun':constraint3}
 con4={'type':'eq','fun':constraint4}
 con5={'type':'eq','fun':constraint5}
 
+# %% Solver
 b=(0,np.inf)
 
 bnds=(b,b,b,b,b,b,b,b,b,b,b,b)
 
 cons=[con1,con2,con3,con4,con5]
-
-#solve using 
-
 
 sol = minimize(obj_func,x0,method='SLSQP',bounds=bnds,constraints=cons)
 
