3ac514e715958f83329d4e16b772b82a2beb134c
vuyohlongwane <57765696+vuyohlongwane@users.noreply.github.com>
2020-04-09 13:52:29 +0200

    Update class_definitions.py

--- class_definitions.py
+++ class_definitions.py
@@ -12,7 +12,7 @@ class client:
     '''
     all input arguments must be annual values
     '''
-    def __init__(self, name='Generic', age='Generic', gross_salary=0, rental_income=0, commission=0, local_dividends=0, 
+    def __init__(self, name='Generic', age=50, gross_salary=0, rental_income=0, commission=0, local_dividends=0, 
                  foreign_dividends=0,interest=0, capital_gains=0, ra_contribution=0):
         self.name=name
         self.age=age
@@ -24,8 +24,7 @@ class client:
         self.interest=interest
         self.capital_gains=capital_gains
         self.ra_contribution=ra_contribution
-        print('1')
-    
+         
     def gross_income(self) :
         gross_income = (self.gross_salary+self.rental_income+self.commission+
                         self.local_dividends+self.foreign_dividends+self.interest+self.capital_gains)
@@ -100,76 +99,25 @@ class client:
 
         return cont
      
-
-
-
-
-        
+       
 def Access_Constraint(x):
-            
-    ME_capital=list(x[56:60])
-    ME_rental=list(x[52:56])
-    ME_interest=list(x[40:44])
-    ME_foreign_dividends=list(x[44:48])
-    ME_local_dividends=list(x[48:52])
-            
-    ME1=ME_local_dividends+ME_foreign_dividends+ME_interest+ME_rental+ME_capital
-            
-            
-    NC_local_dividends=list(x[28:32])
-    NC_interest=list(x[20:24])
-    NC_rental=list(x[32:36])
-    NC_capital=list(x[36:40])
-    NC_foreign_dividends=list(x[24:28])
-            
-    NC1=NC_local_dividends+NC_foreign_dividends+NC_interest+NC_rental+NC_capital
-            
-            
-    KC_rental=list(x[12:16])
-    KC_capital=list(x[16:20])
-    KC_interest=list(x[0:4])
-    KC_foreign_dividends=list(x[4:8])
-    KC_local_dividends=list(x[8:12])
-            
-    KC1=KC_local_dividends+KC_foreign_dividends+KC_interest+KC_rental+KC_capital
-            
-    Ntsika1=list(x[12:53:20])+list(x[8:49:20])+list(x[4:45:20])+list(x[0:41:20])+list(x[16:57:20])
-    Refiloe1=list(x[13:54:20])+list(x[9:50:20])+list(x[5:46:20])+list(x[1:42:20])+list(x[17:58:20])
-    Thando1=list(x[14:55:20])+list(x[10:51:20])+list(x[6:47:20])+list(x[2:43:20])+list(x[18:59:20])
-    Lerato1=list(x[15:56:20])+list(x[11:52:20])+list(x[7:48:20])+list(x[3:44:20])+list(x[19:60:20])
-            
     constraint=[]
-    fund_variables={'NC':NC1,'KC':KC1,'ME':ME1}
-    client_variables={'Ntsika':Ntsika1,'Refiloe':Refiloe1,'Thando':Thando1,'Lerato':Lerato1}
-    
-    client_constraints={'Ntsika':'KC','Thando':'NC','Lerato':'NC'}
-    
-    for i in client_variables['Ntsika']:    
-        if i in fund_variables['KC']:
-            constraint.append(i)
-            
-    for i in client_variables['Thando']:    
-        if i in fund_variables['NC']:
-            constraint.append(i)
-            
-    for i in client_variables['Lerato']:    
-        if i in fund_variables['NC']:
-            constraint.append(i)
-
-  
-               
+    all_funds=['KC','NC','ME']
+    clients=['Ntsika','Refiloe','Thando','Lerato']
+    fund_arguments=5
+        
+    inaccessible_funds={'Ntsika':['KC'],'Thando':['NC'],'Lerato':['NC']}        
+    for client in list(inaccessible_funds.keys()):
+        for item in inaccessible_funds[client]:
+            b=item
+            item=[b]
+            for i in range(0,len(item)):
+                x_0=len(clients)*fund_arguments*all_funds.index(item[i])
+                x_i=list(range(x_0+(clients.index(client)),x_0+len(clients)*int(fund_arguments),len(clients)))
+                for i in x_i:
+                    constraint.append(x[i])                        
     return sum(constraint)
         
-        
-        
-        
-        
-    
-
-
-
-
-
 
 #%%
 class Fund:
@@ -181,16 +129,16 @@ class Fund:
         self.rental=rental
         self.capital_gains=capital_gains
         
+        
     def returns(self):
-        return [self.interest,self.foreign_dividends,
-                self.local_dividends,self.rental,self.capital_gains]
+        return sum([self.interest,self.foreign_dividends,
+                self.local_dividends,self.rental,self.capital_gains])
     
 
-Ntsika=client(name='Ntsika', age=23, ra_contribution=0)    
-Refiloe=client(name='Refiloe', age=22, ra_contribution=0)    
+Ntsika=client(name='Ntsika', age=23, ra_contribution=0, gross_salary=256508)    
+Refiloe=client(name='Refiloe', age=22, ra_contribution=0, gross_salary=72000)    
 Thando=client(name='Thando', age=8, ra_contribution=0)    
 Lerato=client(name='Lerato', age=15,  ra_contribution=0)
-
        
 def obj_func(x):
     Ntsika.rental_income=22055+sum(x[12:53:20])
@@ -218,63 +166,57 @@ def obj_func(x):
     Lerato.capital_gains=sum(x[19:60:20])
        
     total_tax=Ntsika.tax()+Refiloe.tax()+Thando.tax()+Lerato.tax()
-    print(total_tax)
     
-    return total_tax   
-
+    return total_tax
 
 KC=Fund(129715,0,0,110556,0)
 NC=Fund(154,0,0,0,0)
 ME=Fund(0,28000,55000,0,480000) 
-    
 
 def constraint_1(x):
     return sum(x[0:4])-KC.interest
 
-def constraint_2(x):
-    return sum(x[20:24])-NC.interest
-
-def constraint_3(x):
-    return sum(x[40:44])-ME.interest
-
 def constraint_4(x):
     return sum(x[4:8])-KC.foreign_dividends
 
-def constraint_5(x):
-    return sum(x[24:28])-NC.foreign_dividends
-
-def constraint_6(x):
-    return sum(x[44:48])-ME.foreign_dividends
-     
 def constraint_7(x):
     return sum(x[8:12])-KC.local_dividends
-
-def constraint_8(x):
-    return sum(x[28:32])-NC.local_dividends
-
-def constraint_9(x):
-    return sum(x[48:52])-ME.local_dividends        
 
 def constraint_10(x):
     return sum(x[12:16])-KC.rental
 
-def constraint_11(x):
-    return sum(x[32:36])-NC.rental
-
-def constraint_12(x):
-    return sum(x[52:56])-ME.rental
-
 def constraint_13(x):
     return sum(x[16:20])-KC.capital_gains
+
+def constraint_2(x):
+    return sum(x[20:24])-NC.interest
+
+def constraint_5(x):
+    return sum(x[24:28])-NC.foreign_dividends
+
+def constraint_8(x):
+    return sum(x[28:32])-NC.local_dividends
+
+def constraint_11(x):
+    return sum(x[32:36])-NC.rental
 
 def constraint_14(x):
     return sum(x[36:40])-NC.capital_gains
 
 def constraint_15(x):
-    return sum(x[56:60])-ME.capital_gains
- 
-            
+    return sum(x[56:60])-ME.capital_gains   
 
+def constraint_3(x):
+    return sum(x[40:44])-ME.interest    
+
+def constraint_6(x):
+    return sum(x[44:48])-ME.foreign_dividends  
+
+def constraint_9(x):
+    return sum(x[48:52])-ME.local_dividends  
+
+def constraint_12(x):
+    return sum(x[52:56])-ME.rental  
 
 con1={'type':'eq','fun':constraint_1}        
 con2={'type':'eq','fun':constraint_2}          
@@ -294,15 +236,8 @@ con15={'type':'eq','fun':constraint_15}
 con16={'type':'eq','fun':Access_Constraint}
 
 
-
-
-
-
-
 cons=[con1,con2,con3,con4,con5,con6,con7,con8,con9,
       con10,con11,con12,con13,con14,con15,con16]
-
-
 
 b=(0,np.inf)
 
@@ -310,30 +245,12 @@ bnds=tuple([b]*60)
     
 x0=np.array([1000]*60)
 
-
 sol = minimize(obj_func,x0,method='SLSQP',bounds=bnds,constraints=cons)
-
 
 solution_vector=sol.x[:]
 
 np.set_printoptions(suppress=True)
 print(sol.message,end='\n\n')
+print('Tax_payable:',sol.fun)
+print('Overall tax rate:',(sol.fun/(KC.returns()+NC.returns()+ME.returns()))*100)
 print(solution_vector)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
